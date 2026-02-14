794fd8947dc1013329bc7a69fa05e9d40bf475f5
amirrezash0n <shourvarziamirreza@gmail.com>
2026-02-14 22:03:12 +0330

    fix whyus component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-// import Footer from "./components/Footer/Footer";
-import Appointment from "./components/Appointment/Appointment";
-import CallbackForm from "./components/CallbackForm/CallbackForm";
-import Feedbacks from "./components/Feedbacks/Feedbacks";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-import Services from "./components/Services/Services";
 import Stats from "./components/Stats/Stats";
-import WhyUs from "./components/WhyUs/benefitsItem";
+import Services from "./components/Services/Services";
+import Appointment from "./components/Appointment/Appointment";
+import Feedbacks from "./components/Feedbacks/Feedbacks";
+import WhyUs from "./components/WhyUs/BenefitItems";
+import CallbackForm from "./components/CallbackForm/CallbackForm";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
@@ -21,7 +21,7 @@ export default function App() {
         <WhyUs />
         <CallbackForm />
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,3 @@
 export default function Footer() {
-  return <footer>This is a footer</footer>;
+  return <div></div>;
 }
